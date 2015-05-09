263a642d657efcccef666c6b69e3074e9ed9ce84
Camille McMahon <cmm9r2@mail.missouri.edu>
2015-05-09 15:10:55 -0500

    the theme object

---
+++ Newsbies/Newsbies/Theme.swift
@@ -0,0 +1,33 @@
+//
+//  Theme.swift
+//  Newsbies
+//
+//  Created by University of Missouri on 5/9/15.
+//  Copyright (c) 2015 Brent Thoenen. All rights reserved.
+//
+
+import UIKit
+
+class Theme: NSObject, NSCoding {
+    var fontFace: String!
+    var fontColor: UIColor!
+    var fontSize: Double!
+    var backgroundColor: UIColor!
+    
+    required convenience init(coder decoder: NSCoder) {
+        self.init()
+        self.fontFace = decoder.decodeObjectForKey("fontFace") as! String
+        self.fontColor = decoder.decodeObjectForKey("fontColor") as! UIColor
+        self.fontSize = decoder.decodeDoubleForKey("fontSize") as Double
+        self.backgroundColor = decoder.decodeObjectForKey("backgroundColor") as! UIColor
+    }
+    
+    func encodeWithCoder(coder: NSCoder) {
+        coder.encodeObject(self.fontFace, forKey: "fontFace")
+        coder.encodeObject(self.fontColor, forKey: "fontColor")
+        coder.encodeDouble(self.fontSize, forKey: "fontSize")
+        coder.encodeDouble(self.fontSize, forKey: "fontSize")
+        coder.encodeObject(self.backgroundColor, forKey: "backgroundColor")
+    }
+
+}

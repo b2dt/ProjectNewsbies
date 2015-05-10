cf7959f2efb01f41fcefcdb461cffb1811ad0ad0
b2dt <brthoenen@gmail.com>
2015-05-10 16:53:16 -0500

    adding in ContainerViews

---
+++ Newsbies/Newsbies/BackgroundViewController.swift
@@ -0,0 +1,18 @@
+//
+//  BackgroundViewController.swift
+//  Newsbies
+//
+//  Created by Brent Thoenen on 5/10/15.
+//  Copyright (c) 2015 Brent Thoenen. All rights reserved.
+//
+
+import UIKit
+
+class BackgroundViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+}
--- Newsbies/Newsbies/CustomizeViewController.swift
+++ Newsbies/Newsbies/CustomizeViewController.swift
@@ -10,9 +10,41 @@ import UIKit
 
 class CustomizeViewController: UIViewController {
 
+    @IBOutlet weak var sampleText: UILabel!
+    @IBOutlet weak var segmentedControl: UISegmentedControl!
+    @IBOutlet weak var TextView: UIView!
+    @IBOutlet weak var BackgroundView: UIView!
+    @IBOutlet weak var ThemeView: UIView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        sampleText.text = "Text"
+        BackgroundView.hidden=true
+        ThemeView.hidden=true
         // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func indexChanged(sender: UISegmentedControl)
+    {
+        switch segmentedControl.selectedSegmentIndex {
+        case 0:
+            sampleText.text = "Text"
+            TextView.hidden = false
+            BackgroundView.hidden = true
+            ThemeView.hidden=true
+        case 1:
+            sampleText.text = "Background"
+            TextView.hidden = true
+            BackgroundView.hidden = false
+            ThemeView.hidden=true
+        case 2:
+            sampleText.text = "Themes"
+            TextView.hidden = true
+            BackgroundView.hidden = true
+            ThemeView.hidden=false
+        default:
+            break;
+        }
     }
 }

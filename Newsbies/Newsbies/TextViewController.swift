93c8dfdf540bb8e6d9343f5a015c631cb7d0a462
b2dt <brthoenen@gmail.com>
2015-05-10 18:33:26 -0500

    Small formatting update
    fixed small issues

--- Newsbies/Newsbies/TextViewController.swift
+++ Newsbies/Newsbies/TextViewController.swift
@@ -23,9 +23,4 @@ class TextViewController: UIViewController {
         var delegate:ChildTextViewControllerDelegate?
         // Do any additional setup after loading the view.
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
 }
